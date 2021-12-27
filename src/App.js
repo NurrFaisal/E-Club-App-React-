97ed64e2e7f51864802d9f9cf494fafb4611a296
NurFaisal <nurfaisal@nexkraft.com>
2021-12-27 12:11:02 +0600

    .........

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/admin/MasterLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MasterLayout() {
+    return (
+        <div>
+            <h1>Hello Master Layout</h1>
+        </div>
+    )
+}
+
+export default MasterLayout
